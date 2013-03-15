7676ba7baa83b99e47bda46e7d4e3b47a26efc98
Francesco Macagno <fjmacagno1@yahoo.com>
2013-03-15 07:53:47 -0700

    Fixed testing when only compiling

--- src/driver/TurninExecutor.java
+++ src/driver/TurninExecutor.java
@@ -15,7 +15,7 @@ import dbconnection.DatabaseConnection;
  */
 public class TurninExecutor extends Thread {
 	private int turnin_id;
-	
+
 	/**
 	 * Create a new TurninExecutor.
 	 * 
@@ -26,35 +26,36 @@ public class TurninExecutor extends Thread {
 		this.turnin_id = turnin_id;
 		System.out.println("New TurninExec made w/ id=" + turnin_id);
 	}
-	
+
 	/**
 	 * Compiles, runs, and executes this TurninExecutor's turnin.
-	 * @return 
+	 * 
+	 * @return
 	 */
 	public void start() {
 		Turnin t = new Turnin(turnin_id);
 		String s = getCompilableInfo();
-		
+
 		if (s.equals("nothing"))
 			return;
-		
+
 		if (t.compile()) {
+			t.testSource();
+
 			if (s.equals("compile"))
 				return;
 			
-			t.testSource();
 			t.run();
 		}
 	}
-	
+
 	private String getCompilableInfo() {
 		try {
-			String sql =
-					"SELECT `compilable` FROM `turnins` , `assignments` "
-							+ "WHERE turnins.turninid = " + turnin_id + " "
-							+ "AND turnins.assignmentid = assignments.assignmentid";
-			PreparedStatement ps =
-					DatabaseConnection.getConnection().prepareStatement(sql);
+			String sql = "SELECT `compilable` FROM `turnins` , `assignments` "
+					+ "WHERE turnins.turninid = " + turnin_id + " "
+					+ "AND turnins.assignmentid = assignments.assignmentid";
+			PreparedStatement ps = DatabaseConnection.getConnection()
+					.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
 			rs.next();
 			System.out.println("FOUND THAT COMPILABLE IS: " + rs.getString(1));
