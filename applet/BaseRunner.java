11c9eff33b045f568ad53334e8161509aaf959b3
Francesco Macagno <fjmacagno1@yahoo.com>
2013-05-20 11:51:00 -0700

    Fixed Applet

--- applet/BaseRunner.java
+++ applet/BaseRunner.java
@@ -1,15 +1,5 @@
 import java.awt.Color;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.security.AccessController;
@@ -17,189 +7,55 @@ import java.security.PrivilegedAction;
 
 import javax.swing.JApplet;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.text.JTextComponent;
 
 public class BaseRunner extends JApplet {
 
 	private static final long serialVersionUID = -2594949974470065461L;
 
-	JTextField j;
+	JTextArea j;
 
 	@Override
 	public void init() {
 
-		this.rootPane.setSize(900, 900);
+		this.rootPane.setSize(500, 500);
 		this.setForeground(Color.BLUE);
 		
-		j = new JTextField();
+		j = new JTextArea(30, 70);
+		j.setLineWrap(true);
 		
-		j.setSize(900, 900);
+		j.setSize(500, 600);
 		add(j);
+		Console.redirectOutput(j);
 		
-		this.getParameter("");
-
-		log("Downloading...\n");
-		
-		
-		
-		
-		
-		/*
-		AccessController.doPrivileged(new PrivilegedAction<Object>() {
-			public Object run() 
-			{
-				try {
-					
-					URL url = new URL("http://ocean/test/Grader/turnins/5113d173eb709/EvenSum.class");
-					url.openConnection();
-					InputStream reader = url.openStream();
-
-					FileOutputStream writer = new FileOutputStream("EvenSum.class");
-					byte[] buffer = new byte[153600];
-					int bytesRead = 0;
-
-					while ((bytesRead = reader.read(buffer)) > 0) {  
-						writer.write(buffer, 0, bytesRead);
-						buffer = new byte[153600];
-					}
-
-					writer.close();
-					reader.close();
-					
-					FileInputStream f = new FileInputStream("EvenSum.class");
-					String t = "";
-					while(f.available() > 0)
-						t = t + (char)f.read();
-					j.setText(j.getText() + t);
-					//new File("EvenSum.class").deleteOnExit();
-				} catch (Exception c) {
-					j.setText(j.getText() + c.getMessage());
-				}
-				
-				return null;
-			}
-
-			});*/
-		
-		
-		
+		System.out.println("Running program " + getParameter("main"));
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	}
 
 	@Override
 	public void start() {
 
-		/*
 		try {
-			j.setText(j.getText() + "\nRunning....");
-			/*Process p = AccessController.doPrivileged(new PrivilegedAction<Process>() {
-				public Process run() 
-				{
-					 try {
-						return Runtime.getRuntime().exec("java EvenSum");
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						j.setText(e.getMessage());
-					}
-					return null;
-				}
-			});*/
-			
-			//new StreamReader(p.getInputStream(), j);
-			
-			
-			/*j.setText(j.getText() + " Run: " + AccessController.doPrivileged(new PrivilegedAction<Object>() {
-				public Object run() 
-				{
-					try {
-						return ClassLoader.getSystemClassLoader().loadClass("EvenSum").getMethod("main",String[].class).invoke(this, (Object[])null);
-					} catch (Exception e){
-						j.setText(j.getText() + e.getMessage());
-					}
-					return null;
-					
-				}
-			}));
-			
-			/*BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-			// loop through the lines of the output adding them to output
-			String nextLine = br.readLine();
-			while (nextLine != null) {
-				j.setText(j.getText() + " " + nextLine + "\n");
-				nextLine = br.readLine();
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			log(e.getMessage());
-		}*/
-		
-		
-		try {
-			final URL[] urls = {new URL("http://ocean/test/Grader/turnins/5113d173eb709/")};
+			final URL[] urls = {new URL(getParameter("path") + "/")};
 			
 			final URLClassLoader cl = AccessController.doPrivileged(new PrivilegedAction<URLClassLoader>() {
 				public URLClassLoader run() {
 					return new URLClassLoader(urls);
 				}
 			});
-			Class<?> c = cl.loadClass("EvenSum");
+			
+			Class<?> c = cl.loadClass(getParameter("main"));
+			Object[] args = new Object[1];
+			args[0] = new String[0];
 			for(Method m : c.getMethods()){
-				m.invoke(this, (Object[])null);
+				if(m.getName().equals("main"))
+					m.invoke(this, args);
 			}
 			
 			cl.close();
 			
-		} catch(MalformedURLException e){
-			log(e.getMessage());
-		} catch (ClassNotFoundException e) {
-			log(e.getMessage());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
+		} catch(Exception e){
 			e.printStackTrace();
 		}
 
-	}
-	
-	private void log(String s){
-		j.setText(j.getText() + " " + s);
-	}
-	
-	private class StreamReader extends Thread{
-		
-		private JTextComponent j;
-		private InputStream r;
-
-		public StreamReader(InputStream r,JTextComponent j){
-			this.j = j;
-			this.r = r;
-		}
-		
-		public void run(){
-			BufferedReader br = new BufferedReader(new InputStreamReader(r));
-
-			// loop through the lines of the output adding them to output
-			String nextLine;
-			try {
-				nextLine = br.readLine();
-				while (nextLine != null) {
-					j.setText(j.getText() + nextLine + "\n");
-					nextLine = br.readLine();
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		}
 	}
 }
